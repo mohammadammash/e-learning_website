96a2804c79dd875bda3f6adbcee7a6b2c11bc79c
mohammadammash <mohammadammash31@gmail.com>
2022-10-07 16:48:33 +0300

    /backend/database/migrations/2022_10_07_132623_create_users_table.php: Creating migration

---
+++ backend/database/migrations/2022_10_07_132623_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('user_type_id')->references('id')->on('user_types')->onDelete('cascade');
+            $table->string('name');
+            $table->string('profile_url');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
