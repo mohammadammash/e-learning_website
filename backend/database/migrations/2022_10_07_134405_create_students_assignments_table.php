9924d4d771eda511cea5babbe6666f0d21836a9c
mohammadammash <mohammadammash31@gmail.com>
2022-10-07 16:51:22 +0300

    D:/Git/backend/database/migrations/2022_10_07_134405_create_students_assignments_table.php

---
+++ backend/database/migrations/2022_10_07_134405_create_students_assignments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsAssignmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students_assignments', function (Blueprint $table) {
+            $table->foreign('student_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('assignment_id')->references('id')->on('assignments')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students_assignments');
+    }
+}
