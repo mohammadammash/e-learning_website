cbb676e4564f5b0a31de3d96f6f6449df076a947
mohammadammash <mohammadammash31@gmail.com>
2022-10-07 16:50:23 +0300

    /backend/database/migrations/2022_10_07_133907_create_assignments_table.php: Creating Assignments Migration

---
+++ backend/database/migrations/2022_10_07_133907_create_assignments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssignmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+            $table->string('file_url');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assignments');
+    }
+}
