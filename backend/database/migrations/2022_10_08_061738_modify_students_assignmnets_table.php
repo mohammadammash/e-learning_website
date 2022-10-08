8b9d96d2a9e887433483192fc67a2d50eb44e065
mohammadammash <mohammadammash31@gmail.com>
2022-10-08 09:22:30 +0300

    /backend/database/migrations/2022_10_08_061738_modify_students_assignmnets_table.php: Adding Modify Students Assignments Table

--- backend/database/migrations/2022_10_08_061738_modify_students_assignmnets_table.php
+++ backend/database/migrations/2022_10_08_061738_modify_students_assignmnets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStudentsAssignmentsTable extends Migration
+class ModifyStudentsAssignmnetsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,9 +14,7 @@ class CreateStudentsAssignmentsTable extends Migration
     public function up()
     {
         Schema::create('students_assignments', function (Blueprint $table) {
-            $table->foreign('student_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('assignment_id')->references('id')->on('assignments')->onDelete('cascade');
-            $table->timestamps();
+            $table->string('file_url');
         });
     }
 
@@ -27,6 +25,6 @@ class CreateStudentsAssignmentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('students_assignments');
+        //
     }
 }
