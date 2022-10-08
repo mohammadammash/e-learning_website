92a0d51fe57a6fa5865921be94dafe38ef1658ca
mohammadammash <mohammadammash31@gmail.com>
2022-10-08 16:44:27 +0300

    backend/app/Models/Assignment.php: Adding Eloquent Models

--- backend/app/Models/Assignment.php
+++ backend/app/Models/Assignment.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
 
 class Assignment extends Model
 {
     use HasFactory;
     protected $fillable = [
+        'course_id',
         'file_url',
         'name',
     ];
