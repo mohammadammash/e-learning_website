7c6e5471c9bccf75aa78f39da08de9b87adaf707
mohammadammash <mohammadammash31@gmail.com>
2022-10-08 10:36:07 +0300

    backend/app/Http/Kernel.php: Adding specific names to each middleware

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -63,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \App\Http\Middleware\CheckIfAdmin::class,
+        'instructor' => \App\Http\Middleware\CheckIfInstructor::class,
+        'student' => \App\Http\Middleware\CheckIfStudent::class,
     ];
 }
--- backend/config/auth.php
+++ backend/config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'api',
+        'guard' => 'auth',
         'passwords' => 'users',
     ],
 
@@ -40,7 +40,7 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-        'api' => [
+        'auth' => [
             'driver' => 'jwt',
             'provider' => 'users',
         ],
