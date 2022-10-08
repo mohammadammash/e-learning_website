571c1120fca53524c4f2bf09ca32f14193795930
mohammadammash <mohammadammash31@gmail.com>
2022-10-08 16:45:15 +0300

    backend/app/Models/Usertype.php: Adding Eloquent Models

--- backend/app/Models/Usertype.php
+++ backend/app/Models/Usertype.php
@@ -3,14 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
 
 class Usertype extends Model
 {
     use HasFactory;
+ 
+    protected $table = 'user_types';
+
     protected $fillable = [
-        'id',
         'type',
     ];
 
+    public function users(){
+        return $this->hasMany(User::class, 'user_type_id', 'id');
+    }
 }
