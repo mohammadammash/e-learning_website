e4eb114374d4da89b2b08eb9e6b03e73d93757a1
mohammadammash <mohammadammash31@gmail.com>
2022-10-08 10:42:15 +0300

    backend/routes/api.php: Creating admin routes inside admin authorization middleware

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,13 +11,17 @@ Route::group(['prefix' => 'v0.1'], function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
 
-    //JWT-AUTH MIDDLEWARE
+    //JWT-AUTH MIDDLEWARE //ALL USERS SHOULD BE AUTHENTICATED
     Route::group(['middleware' => 'auth'], function () {
         Route::post('refresh', [AuthController::class, 'refresh']);
         Route::post('me', [AuthController::class, 'me']);
 
         //ADMIN  AUTHORIZATION
         Route::group(['middleware' => 'admin'], function () {
+            Route::post('/instructor/add',[AdminController::class,'addInstructor'])->name('add-instructor');
+            Route::post('/student/add',[AdminController::class,'addStudent'])->name('add-student');
+            Route::post('/course/add',[AdminController::class,'addCourse'])->name('add-course');
+            Route::post('/instructor/course/assign', [AdminController::class, 'assignInstructorToCourse'])->name('assign-instructor-to-course');
         });
 
         //STUDENT AUTHORIZATION
