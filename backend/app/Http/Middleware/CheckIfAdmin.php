7068731de5a9636ba0d07ddb671c9d6fca88dd39
mohammadammash <mohammadammash31@gmail.com>
2022-10-08 16:38:39 +0300

    backend/app/Http/Middleware/CheckIfAdmin.php: Finalizing functionality of the Middleware

--- backend/app/Http/Middleware/CheckIfAdmin.php
+++ backend/app/Http/Middleware/CheckIfAdmin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use App\Models\Usertype;
 
 class CheckIfAdmin
 {
@@ -17,12 +18,11 @@ class CheckIfAdmin
 
     public function handle(Request $request, Closure $next)
     {
-        $credentials = request(['email', 'password', 'user_type_id']);
+        $usertype = Usertype::find(auth()->user());
+        $type = $usertype[0]->type;
 
-        // if (!$token = auth()->attempt($credentials)) {
-        // return response()->json(['error' => 'Unauthorized'], 401);
-        // }
-        if(!$request) return;
+        if ($type !== "admin")
+            return response()->json("Unauthorized", 403);
 
         return $next($request);
     }
