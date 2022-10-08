f42b702ab29682e62f12746783bb52bd88bac6d9
mohammadammash <mohammadammash31@gmail.com>
2022-10-08 20:31:13 +0300

    backend/app/Http/Controllers/StudentController.php:Editing courses to get courses info

--- backend/app/Http/Controllers/StudentController.php
+++ backend/app/Http/Controllers/StudentController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\StudentCourse;
+use App\Models\Course;
 
 class StudentController extends Controller
 {
     public function getCourses(Request $request)
     {
-        return 'coursetdfo';
+        $user = auth()->user();
+        $student_id = $user->id;
+        $student_courses = $user->students_courses()->get();
+        $courses = [];
+
+        foreach($student_courses as $std){
+            array_push($courses,$std->courses()->get());
+        }
+        
+        return response()->json([
+                'status'=>'Success',
+                "data"=>$courses
+            ]);
     }
 
     public function submitAssignment(Request $request)
