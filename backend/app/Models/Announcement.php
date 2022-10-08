58a7c716993f21e2fabe71863d1fcb1dc713c782
mohammadammash <mohammadammash31@gmail.com>
2022-10-08 16:44:15 +0300

    backend/app/Models/Announcement.php: Adding Eloquent Models

--- backend/app/Models/Announcement.php
+++ backend/app/Models/Announcement.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
+
 
 class Announcement extends Model
 {
     use HasFactory;
     protected $fillable = [
+        'instructor_id',
         'content',
     ];
 
