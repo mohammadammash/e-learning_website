a84090c62c721b89b2f461da725c42c3f0453000
mohammadammash <mohammadammash31@gmail.com>
2022-10-09 13:17:15 +0300

    backend/app/Http/Controllers/AdminController.php/AddStudent(): Adding new student

--- backend/app/Http/Controllers/AdminController.php
+++ backend/app/Http/Controllers/AdminController.php
@@ -4,22 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+
 class AdminController extends Controller
 {
-    public function addStudent(Request $request){
-        $user = auth()->user();
-        return $user;
+    public function addStudent(Request $request)
+    {
+        $res = app('App\Http\Controllers\AuthController')->register($request, env('STUDENT_USER_TYPE_ID'));
+        if ($res) {
+            return response()->json([
+                'status' => 'Success',
+                'data' => 'New Student Added',
+            ]);
+        }
+
+        return response()->json([
+            'status' => 'Success',
+            'data' => 'Student Not Added',
+        ]);
     }
 
-    public function addInstructor(Request $request){
-        return 'instructero';
+    public function addInstructor(Request $request)
+    {
+
     }
 
-    public function addCourse(Request $request){
+    public function addCourse(Request $request)
+    {
         return 'Coursetoo';
     }
 
-    public function assignInstructorToCourse(Request $request){
+    public function assignInstructorToCourse(Request $request)
+    {
         return 'assignetoo';
     }
 }
