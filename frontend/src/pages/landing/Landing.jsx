ce86f5e1ba104de81f9426b3c684dcc5b866d203
mohammadammash <mohammadammash31@gmail.com>
2022-10-09 20:08:37 +0300

    frontend/src/pages/landing/Landing.jsx: Adding html content for the left section

--- frontend/src/pages/landing/Landing.jsx
+++ frontend/src/pages/landing/Landing.jsx
@@ -6,12 +6,14 @@ export default function Landing() {
   return (
     <div className="main-landing">
       <section className="left-section">
+        <div className="content">
         <p class='top-text'>E-learning is a better way of Learning</p>
         <div className="middle">
-          <label>Already a user?</label>
-          <button className="show-login"></button>
+          <label>Already A User?</label>
+          <button className="show-login-button">Login</button>
         </div>
         <p class='bottom-text'>Are you a student but without an account? Reach out to your instructor, then revisit us with your created account</p>
+      </div>
       </section>
       <section className="right-section">
         <img className="landing-img" src={img} alt="landing-img1" />
