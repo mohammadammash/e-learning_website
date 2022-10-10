c98cc823e8a31f53b32aab9cd05b21a89266fca8
mohammadammash <mohammadammash31@gmail.com>
2022-10-10 08:26:10 +0300

    frontend/src/pages/landing/Landing.jsx: Commenting and cleaning

--- frontend/src/pages/landing/Landing.jsx
+++ frontend/src/pages/landing/Landing.jsx
@@ -7,20 +7,21 @@ import Popup from "../../components/Popup";
 
 
 export default function Landing() {
-    const [hidden, setHidden] = useState(true);
+    const [hidden, setHidden] = useState(true); //state to show/hide login popup
 
-    const toggleHide = () => {
-      setHidden(hidden=>!hidden);
+    const toggleHide = () => { //parent function to let children & parent toggle popup modal
+      setHidden(hidden=>!hidden); //toggle last state
     };
 
   return (
     <>
       <div className="main-landing">
+
         <section className="left-section">
           <div className="content">
             <p className="top-text">E-learning is a better way of Learning</p>
             <div className="middle">
-              <Button label={"Already A User?"} text={"Login"} toggleHide={toggleHide}/>
+              <Button label={"Already A User?"} text={"Login"} toggleHide={toggleHide} />
             </div>
             <p className="bottom-text">Are you a student but without an account? Reach out to your instructor, then revisit us with your created account</p>
           </div>
@@ -29,9 +30,10 @@ export default function Landing() {
         <section className="right-section">
           <img className="landing-img" src={img} alt="landing-img1" />
         </section>
+        
       </div>
 
-      <Popup modal_type={"login"} hidden={hidden} toggleHide={toggleHide}/>
+      <Popup modal_type={"login"} hidden={hidden} toggleHide={toggleHide} />
     </>
   );
 }
