ca5334cefc0b9a38faa6b5ea776ebb375a385bc1
mohammadammash <mohammadammash31@gmail.com>
2022-10-10 22:02:22 +0300

    frontend/src/components/Navbar.jsx: Linking Home button and adding basic logout functionality

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,23 @@
 import Button from "./Button";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
+    const navigate = useNavigate();
     const logoutUser = ()=>{
-        console.log('logged out!');
+        localStorage.clear();
+        navigate('/');
     }
+
 
   return (
     <>
       <nav className="nav">
         <h1 className="nav-logo">EDULAB</h1>
         <div className="nav-links">
-        <a className="nav-link" href="#ds">
-          HOME/COURSES
-        </a>
+        <Link to='/home' className="nav-link" href="#ds">
+          HOME
+        </Link>
         <Button  text='logout' changeState={logoutUser}/>
         </div>
       </nav>
