9aa087397f6b1e2dbfe6b665535ac2ba1f1b595a
mohammadammash <mohammadammash31@gmail.com>
2022-10-10 22:54:40 +0300

    frontend/src/pages/student/home/Home.jsx: Mapping through courses and showing them to the student

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
 //COMPONENTS:
 import Landing from "./pages/landing/Landing.jsx";
-import Home from"./pages/student/home/Home.jsx";
+import Home from "./pages/student/home/Home.jsx";
 
 function App() {
   return (
--- frontend/src/pages/student/home/Home.jsx
+++ frontend/src/pages/student/home/Home.jsx
@@ -19,26 +19,24 @@ export default function Home() {
       <Navbar />
 
       <section className="courses-container">
-        <div className="course-card">
-          <h1>Course Title</h1>
-          <h3>Instructor Name</h3>
-          <p className="course-name">
-            <span>COURSE NAME: </span>Describe yourself in one sentence” is the other way to ask the short answer question. Take note of the summary above and use the three-word description as part of a summary sentence. This way you’ll have an answer to both questions with the same words.
-          </p>
-          <p>
-            CREDITS: <span className="course-credits">5</span>
-          </p>
-        </div>
-        <div className="course-card">
-          <h1>Course Title</h1>
-          <h3>Instructor Name</h3>
-          <p className="course-name">
-            <span>COURSE NAME: </span>Describe yourself in one sentence” is the other way to ask the short answer question. Take note of the summary above and use the three-word description as part of a summary sentence. This way you’ll have an answer to both questions with the same words.
-          </p>
-          <p>
-            CREDITS: <span className="course-credits">5</span>
-          </p>
-        </div>
+        {courses.length <= 0 ? (
+          <h1>Loading</h1>
+        ) : (
+          <>
+            {courses.map(({ _id, name, description, credits }) => {
+              return (
+                <div className="course-card" key={_id}>
+                  <h1>Course: {name}</h1>
+                  <h2>Course Description: </h2>
+                  <p className="course-description">{description}</p>
+                  <p>
+                    CREDITS: <span className="course-credits">{credits}</span>
+                  </p>
+                </div>
+              );
+            })}
+          </>
+        )}
       </section>
     </>
   );
