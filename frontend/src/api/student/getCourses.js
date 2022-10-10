4e59134bed8c39c3493dccb1aebfc84b6154e2f7
mohammadammash <mohammadammash31@gmail.com>
2022-10-10 22:31:25 +0300

    frontend/src/api/student/: Creating new api to retrieve all courses for student

---
+++ frontend/src/api/student/getCourses.js
@@ -0,0 +1,14 @@
+export const studentCourses_getAPI = async (token) => {
+  const api_url = `${window.baseURL}/courses`;
+
+  try {
+    const response = await window.getAPI(api_url, token);
+    if (!response) return false; //courses not found
+
+    if (response && response.status === 200) {
+      return response.data.data;
+    }
+  } catch (err) {
+    return "DATA NOT RETIREVED";
+  }
+};
