ac3cbd15572b739dc8054853603c63c47097fbc4
mohammadammash <mohammadammash31@gmail.com>
2022-10-10 08:23:08 +0300

    frontend/src/pages/landing/Landing.jsx: Creating a State to show Hide login popup

--- frontend/src/pages/landing/Landing.jsx
+++ frontend/src/pages/landing/Landing.jsx
@@ -1,19 +1,26 @@
 import "./landing.css";
 import img from "../../assets/Happy Bunch - Desk.png";
+import { useState } from "react";
 //Components
 import Button from "../../components/Button";
 import Popup from "../../components/Popup";
 
+
 export default function Landing() {
+    const [hidden, setHidden] = useState(true);
+
+    const toggleHide = () => {
+      setHidden(hidden=>!hidden);
+    };
+
   return (
     <>
       <div className="main-landing">
-
         <section className="left-section">
           <div className="content">
             <p className="top-text">E-learning is a better way of Learning</p>
             <div className="middle">
-              <Button label={"Already A User?"} text={"Login"} />
+              <Button label={"Already A User?"} text={"Login"} toggleHide={toggleHide}/>
             </div>
             <p className="bottom-text">Are you a student but without an account? Reach out to your instructor, then revisit us with your created account</p>
           </div>
@@ -23,8 +30,8 @@ export default function Landing() {
           <img className="landing-img" src={img} alt="landing-img1" />
         </section>
       </div>
-      
-      <Popup modal_type={'login'}/>
+
+      <Popup modal_type={"login"} hidden={hidden} toggleHide={toggleHide}/>
     </>
   );
 }
