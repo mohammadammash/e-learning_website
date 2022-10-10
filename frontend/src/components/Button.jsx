de0c4b894db217dd884e2efb04092bf21eb72bab
mohammadammash <mohammadammash31@gmail.com>
2022-10-10 08:21:37 +0300

    frontend/src/components/Button.jsx: Adding onClick Event listener to show login modal

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,8 +1,8 @@
-export default function Button({label, text}) {
+export default function Button({label, text, toggleHide}) {
   return (
     <>
       <label>{label}</label>
-      <button className="show-login-button btn">{text}</button>
+      <button className="show-login-button btn" onClick={toggleHide}>{text}</button>
     </>
   );
 }
