5e9e568714cd35b79f2bd0618ad823c888437e92
mohammadammash <mohammadammash31@gmail.com>
2022-10-10 15:18:12 +0300

    frontend/src/api/: Handling the login postAPI in login.js api file

--- frontend/src/pages/landing/Landing.jsx
+++ frontend/src/pages/landing/Landing.jsx
@@ -2,6 +2,8 @@ import "./landing.css";
 import img from "../../assets/Happy Bunch - Desk.png";
 import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
+//api
+import { loginUser_postAPI } from "../../api/login";
 //Components
 import Button from "../../components/Button";
 import Popup from "../../components/Popup";
@@ -13,47 +15,20 @@ export default function Landing() {
     setHidden((hidden) => !hidden); //toggle last state
   };
 
-  //login inputs:
   const email = useRef(0);
   const password = useRef(0);
-  const navigate = useNavigate(); //to render routes
+  const navigate = useNavigate();
 
   const submitLogin = async (e) => {
     e.preventDefault();
-    //validation
     if (email.current.value === "" || password.current.value === "") {
       console.log("EMPTY");
       return;
     }
     //post login user
-    const loginURL = `${window.baseURL}/login`;
-    const data = {
-      email: email.current.value,
-      password: password.current.value,
-    };
-    try {
-      const response = await window.postAPI(loginURL, data);
-      if(response.status === 200){
-        const user = response.data.data;
-        if(user.user_type === 'student'){
-          console.log('HE IS A STUDENT');
-          localStorage.setItem("token", JSON.stringify(user.token));
-          navigate('/student');
-        }
-        else if(user.user_type === 'instructor'){
-          console.log('HE IN A INSTRUCTOR');
-          localStorage.setItem("token", JSON.stringify(user.token));
-          navigate("/instructor");
-        }
-        else if(user.user_type === 'admin'){
-          console.log("HE IN A ADMIN");
-          localStorage.setItem("token", JSON.stringify(user.token));
-          navigate("/admin");
-        }
-      }
-    } catch (err) {
-      console.log(err);
-    }
+    const data =  await loginUser_postAPI(email.current.value, password.current.value);
+    const user = { name:data.name, email:data.email, profile_url:data.profile_url, token:data.token };
+    localStorage.setItem('user',JSON.stringify(user));
   };
 
   return (
