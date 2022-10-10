bf3d6b35eb5bfdc5c0f32f3f4f6f57fb2dae1d80
mohammadammash <mohammadammash31@gmail.com>
2022-10-10 08:22:11 +0300

    frontend/src/components/Popup.jsx: Adding onClick Event to hide modal when (X) clicked

--- frontend/src/components/Popup.jsx
+++ frontend/src/components/Popup.jsx
@@ -1,23 +1,28 @@
 import Input from "./Input";
 import Button from "./Button";
 
-export default function Popup({ modal_type }) {
-  return (
-    <section className="modal">
-      <div className="modalContent">
-        <button className="close-button">✖</button>
-        <h2 className="capitalize">{modal_type}</h2>
-        <form className="form-group">
+export default function Popup({ modal_type, hidden, toggleHide }) {
+
+  if (!hidden) {
+    return (
+      <section className="modal">
+        <div className="modalContent">
+          <button className="close-button" onClick={toggleHide}>
+            ✖
+          </button>
+          <h2 className="capitalize">{modal_type}</h2>
+          <form className="form-group">
             {/* if the popup type is login: show inputs and button */}
-          {modal_type === "login" && (
-            <>
-              <Input classes={"form-input"} type={"email"} placeholder={"email"} />
-              <Input classes={"form-input"} type={"password"} placeholder={"password"} />
-              <Button text={"Submit"} />
-            </>
-          )}
-        </form>
-      </div>
-    </section>
-  );
+            {modal_type === "login" && (
+              <>
+                <Input classes={"form-input"} type={"email"} placeholder={"email"} />
+                <Input classes={"form-input"} type={"password"} placeholder={"password"} />
+                <Button text={"Submit"} />
+              </>
+            )}
+          </form>
+        </div>
+      </section>
+    );
+  }
 }
