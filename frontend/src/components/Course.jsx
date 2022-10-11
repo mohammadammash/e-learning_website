fe470251b139444dc79b10293d3b7d7284822773
mohammadammash <mohammadammash31@gmail.com>
2022-10-11 20:59:05 +0300

    frontend/src/components/Course.jsx: Passing showassignment function to the button modal

--- frontend/src/components/Course.jsx
+++ frontend/src/components/Course.jsx
@@ -1,14 +1,17 @@
-export default function Course(data) {
-    const {name, description, credits} = data.data;
+import Button from "./Button";
+
+export default function Course(props) {
+  const [{ _id, name, description, credits },showAssignment] = [props.data,props.showAssignment];
+
   return (
-    <div className="course-card" >
+    <div className="course-card">
       <h1>Course: {name}</h1>
       <h2 className="course-description-title">Course Description: </h2>
       <p className="course-description">{description}</p>
       <p>
         CREDITS: <span className="course-credits">{credits}</span>
       </p>
-      <button className="btn">Show assignments</button>
+      <Button text={'show assignments'} type='showAssignment' changeState={showAssignment} task={{_id,name}}/>
     </div>
   );
 }
