41a4653e66961affc88ccf9d0d42142256a702e2
mohammadammash <mohammadammash31@gmail.com>
2022-10-11 23:35:52 +0300

    frontend/src/pages/instructor/Home.jsx: Adding function to useEffect so whenever page loads and user is instructor get courses

--- frontend/src/pages/instructor/Home.jsx
+++ frontend/src/pages/instructor/Home.jsx
@@ -2,18 +2,39 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 //Components:
 import Navbar from "../../components/Navbar";
+import {instructorCourses_getAPI} from "../../api/getInstructorCourses";
+import { useState } from "react";
 
 export default function Home() {
+  const navigate = new useNavigate();
+  const [courses, setCourses] = useState([]);
+
+  const getInstructorCourses = async () => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    const data = await instructorCourses_getAPI(user.token);
+    if (!data) {
+      //unauthenticated - token expired (return false from fetch get)
+      localStorage.clear();
+      navigate("/");
+    }
+    console.log(data);
+    setCourses(data);
+  };
 
   useEffect(() => {
-    console.log("MY INSTRUCTOR");
+    getInstructorCourses();
   }, []);
 
   return (
     <>
       <Navbar />
-
-      <h1>My Instructor</h1>
+      {courses.length <= 0 ? (
+        <div className="spinner-container">
+          <div className="loading-spinner"></div>
+        </div>
+      ) : (
+        <h1>My Instructor</h1>
+      )}
     </>
   );
 }
