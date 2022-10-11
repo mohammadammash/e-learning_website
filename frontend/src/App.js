86419ea044bc6b3c3174744655853f26fdef085e
mohammadammash <mohammadammash31@gmail.com>
2022-10-11 22:47:24 +0300

    frontend/src/App.js: Importing and adding the instructor home route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,18 @@ import { useState, useEffect } from "react";
 import Landing from "./pages/Landing.jsx";
 import Home from "./pages/student/Home.jsx";
 import Assignment from "./pages/student/Assignment.jsx";
-import Navbar from "./components/Navbar.jsx";
+import InstructorHome from "./pages/instructor/InstructorHome.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />}></Route>
+        {/* STUDENT ROUTES */}
         <Route path="/home" element={<Home />}></Route>
         <Route path="/home/:course_name" element={<Assignment />}></Route>
+        {/* INSTRUCTOR ROUTES */}
+        <Route path="/instructor/home" element={<InstructorHome />}></Route>
       </Routes>
     </BrowserRouter>
   );
