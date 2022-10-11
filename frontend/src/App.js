2159a5af62d4efd54dbab6193f79c050a46e23fd
mohammadammash <mohammadammash31@gmail.com>
2022-10-11 21:00:31 +0300

    frontend/src/App.js: Adding Route to navigate to single assignment page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 //COMPONENTS:
 import Landing from "./pages/Landing.jsx";
 import Home from "./pages/student/Home.jsx";
+import Assignment from "./pages/student/Assignment.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />}></Route>
         <Route path="/home" element={<Home />}></Route>
+        <Route path="/home/:course_name" element={<Assignment />}></Route>
       </Routes>
     </BrowserRouter>
   );
