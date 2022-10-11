8c908d19933a773c0c60e87c295b489a3cbf04e8
mohammadammash <mohammadammash31@gmail.com>
2022-10-11 23:21:13 +0300

    frontend/src/App.js: Adding route for admin home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,23 @@ import Landing from "./pages/Landing.jsx";
 import StudentHome from "./pages/student/Home.jsx";
 import Assignment from "./pages/student/Assignment.jsx";
 import InstructorHome from "./pages/instructor/Home.jsx";
+import AdminHome from "./pages/admin/Home.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />}></Route>
+
         {/* STUDENT ROUTES */}
         <Route path="/home" element={<StudentHome />}></Route>
         <Route path="/home/:course_name" element={<Assignment />}></Route>
+
         {/* INSTRUCTOR ROUTES */}
         <Route path="/instructor/home" element={<InstructorHome />}></Route>
+
+        {/* ADMIN ROUTES */}
+        <Route path="/admin/home" element={<AdminHome />}></Route>
       </Routes>
     </BrowserRouter>
   );
